affcb7e07c2d3b8b5bff04fde10b5199925c7b97
Fernando A <fernando@nemesis.(none)>
2011-01-21 10:35:02 +0100

    implemented: getRbacObjectFromRule, getRbacActionsFromRule

--- Classes/Services/ZendAccessControllService.php
+++ Classes/Services/ZendAccessControllService.php
@@ -8,7 +8,8 @@ class Tx_Rbac_Service_ZendAccessControlService implements Tx_Rbac_AccessControlS
 		* @return string The resource object string "ObjectA"
 		**/
 		protected function getRbacObjectFromRule($rbacRule) {
-
+			$ruleArray = preg_split('/>/',$rbacRule);
+			return trim($ruleArray[0]) ? strtolower(trim($ruleArray[0])): array();
 		}
 
 		/**
@@ -17,7 +18,18 @@ class Tx_Rbac_Service_ZendAccessControlService implements Tx_Rbac_AccessControlS
 		* @return array The actions of the resource object array('new','edit','delete')
 		**/
 		protected function getRbacActionsFromRule($rbacRule) {
+			$actionsResult = array();
+			$ruleArray = preg_split('/>/',$rbacRule);
+			if (isset($ruleArray[1])) {
+				$actionsArray = preg_split('/,/',$ruleArray[1]);
+				foreach ($actionsArray as $actions) {
+					if (trim($actions)) {
+						$actionsResult[] = strtolower(trim($actions));
+					}
+				}
+			}
 
+			return $actionsResult;
 		}
 
 		public function hasAccess($feUser, $rbacRule) {
