69c47fbd0515a28245d47e84a1c9110106721121
Fernando Arconada <fernando.arconada@gmail.com>
2011-01-22 00:35:45 +0100

    isValidRuleSyntax

--- Classes/Services/ZendAccessControlService.php
+++ Classes/Services/ZendAccessControlService.php
@@ -37,7 +37,7 @@ class Tx_Rbac_Service_ZendAccessControlService implements Tx_Rbac_AccessControlS
 
 			return $actionsResult;
 		}
-		
+
 		/**
 		* Retrurns true or false depending if a user is allowed to access the object's actions
 		* @param Tx_Extbase_Domain_Repository_FrontendUserRepository $feUser FrontEnd user
@@ -45,12 +45,9 @@ class Tx_Rbac_Service_ZendAccessControlService implements Tx_Rbac_AccessControlS
 		*
 		**/
 		public function hasAccess($feUser, $rbacRule) {
-			if(!get_class($feUser) == 'Tx_Extbase_Domain_Repository_FrontendUserRepository'){
-				return FALSE;
-			}
 			if(!is_array($rbacRule)){
 				// always think that you have an array of rules
-				$rbacRule = array($rbacRule);		
+				$rbacRule = array($rbacRule);
 			}
 			$this->setFeUser($feUser);
 			$this->userAcl = getUserAcl();
@@ -58,9 +55,9 @@ class Tx_Rbac_Service_ZendAccessControlService implements Tx_Rbac_AccessControlS
 		}
 
 		protected function getUserAcl(){
-			
+
 		}
-		
+
 		protected function evalOneRbacRule($rbacRule){
 
 		}
@@ -68,13 +65,23 @@ class Tx_Rbac_Service_ZendAccessControlService implements Tx_Rbac_AccessControlS
 		protected function evalAllRbacRules($rbacRules){
 			$isAllowed = TRUE;
 			while ($rbacRule=array_pop($rbacRules) && $isAllowed == TRUE) {
+				if( !$this->isValidRuleSyntax($rule)) {
+					throw new Tx_Rbac_AccessControlServiceException('error: is not valid rule syntax');
+				}
 				$isAllowed = $this->evalOneRbacRule($rbacRule);
 			}
 			return $isAllowed;
 		}
-		
+
 		protected function setFeUser($feUser) {
+			if(!get_class($feUser) == 'Tx_Extbase_Domain_Repository_FrontendUserRepository'){
+				throw new Tx_Rbac_AccessControlServiceException('error: is not valid FE user of type Tx_Extbase_Domain_Repository_FrontendUserRepository');
+			}
 			$this->feUser = $feUser;
+		}
+
+		protected function isValidRuleSyntax($rule) {
+			return preg_match('/[[:alnum:]][\t\s]*>([\t\s]*[[:alnum:]*]+[\t\s]*[,]*)+$/', $rule);
 		}
 
 }
