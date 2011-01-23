8575b051de86f43073ec7fd4d3a9d82eb7cf6d32
Fernando Arconada <fernando.arconada@gmail.com>
2011-01-23 12:58:27 +0100

    PHPdoc and parsed * rules

--- Classes/Service/ZendAccessControlService.php
+++ Classes/Service/ZendAccessControlService.php
@@ -10,17 +10,26 @@ class Tx_Rbac_Service_ZendAccessControlService implements Tx_Rbac_Interface_Acce
 		*/
 		protected $feUser;
 
+		/*
+		 * @var Zend_Acl
+		 */
 		protected $userAcl;
 
+		/*
+		 * @var string Name of the extension managed: used to retrieve the user TS
+		 */
 		protected $extensionName;
 
+		/*
+		 * @var array Controller Settings: to retrieve the page-TS level defined roles
+		 */
 		protected $pluginSettings;
 
 
 		/**
 		* Gets a string with the resource object
 		* @param string $rbacRule "@rbacRule ObjectA > new,edit,delete"
-		* @return string The resource object string "ObjectA"
+		* @return string The resource object string "objecta" trimed and lowercased
 		**/
 		protected function getRbacObjectFromRule($rbacRule) {
 			$ruleArray = preg_split('/>/',$rbacRule);
@@ -30,7 +39,7 @@ class Tx_Rbac_Service_ZendAccessControlService implements Tx_Rbac_Interface_Acce
 		/**
 		* Gets an array with the actions related to a resource object
 		* @param string $rbacRule "@rbacRule ObjectA > new,edit,delete"
-		* @return array The actions of the resource object array('new','edit','delete')
+		* @return array The actions of the resource object array('new','edit','delete') trimed and lowercased
 		**/
 		protected function getRbacActionsFromRule($rbacRule) {
 			$actionsResult = array();
@@ -49,8 +58,11 @@ class Tx_Rbac_Service_ZendAccessControlService implements Tx_Rbac_Interface_Acce
 
 		/**
 		* Retrurns true or false depending if a user is allowed to access the object's actions
+		* The extension name should be set with setExtensionName()
 		* @param Tx_Extbase_Domain_Repository_FrontendUserRepository $feUser FrontEnd user
 		* @param mixed $rbacRule "ObjectA > new,edit,delete", could be an array of rules
+		* @throws Tx_Rbac_Exception_AccessControlServiceException
+		* @return bool
 		*
 		**/
 		public function hasAccess($feUser, $rbacRule) {
@@ -59,10 +71,15 @@ class Tx_Rbac_Service_ZendAccessControlService implements Tx_Rbac_Interface_Acce
 				$rbacRule = array($rbacRule);
 			}
 			$this->setFeUser($feUser);
-			$this->userAcl = $this->getUserAcl();
+			$this->buildUserAcl();
 			return $this->evalAllRbacRules($rbacRule);
 		}
 
+		/**
+		 * Get the Rbac configured roles at FE us TS
+		 *
+		 * @return array
+		 */
 		private function getUserRolesFromTS() {
 			$userTs = $this->feUser->getUserTSconf();
 			if(isset($userTs['plugin.']['tx_'.$this->extensionName.'.']['settings.']['rbac.']['roles.'])){
@@ -73,6 +90,11 @@ class Tx_Rbac_Service_ZendAccessControlService implements Tx_Rbac_Interface_Acce
 
 		}
 
+		/**
+		 * Get the roles defined at page TS
+		 *
+		 * @return array
+		 */
 		private function getPluginRolesFromTS(){
 			if(isset($this->pluginSettings['rbac']['roles'])) {
 				return $this->pluginSettings['rbac']['roles'];
@@ -80,21 +102,30 @@ class Tx_Rbac_Service_ZendAccessControlService implements Tx_Rbac_Interface_Acce
 				return array();
 			}
 		}
-		private function getAppliedRoles() {
+
+		/**
+		 * Get the role applied to the user
+		 * Only one role per user
+		 *
+		 * @return string Role applied lowercased and trimed
+		 */
+		private function getAppliedRole() {
 			$userTs = $this->feUser->getUserTSconf();
-			//t3lib_div::debug($userTs['plugin.']['tx_'.$this->extensionName.'.']['settings.']);
-			$appliedRoles = Tx_Extbase_Utility_Arrays::trimExplode(',', $userTs['plugin.']['tx_'.$this->extensionName.'.']['settings.']['rbac.']['applyRoles'],TRUE);
-			$result = array();
-			foreach($appliedRoles as $role){
-				$result[] = strtolower(trim($role));
-			}
-			return $result;
+			$role = $userTs['plugin.']['tx_'.$this->extensionName.'.']['settings.']['rbac.']['appliedRole'];
+
+			return strtolower(trim($role));
 		}
 
-		protected function getUserAcl(){
+		/**
+		 * Builds the user ACL database and sets $this->userAcl;
+		 *
+		 * @throws Tx_Rbac_Exception_AccessControlServiceException
+		 */
+		protected function buildUserAcl(){
 			$acl = new Zend_Acl();
+			// All roles defined at page and user level
 			$roles = Tx_Extbase_Utility_Arrays::arrayMergeRecursiveOverrule($this->getPluginRolesFromTS(), $this->getUserRolesFromTS());
-			t3lib_div::debug($roles);
+			//t3lib_div:debug($roles);
 			foreach ($roles as $roleName => $roleValues) {
 				try {
 					// create the roles
@@ -121,12 +152,21 @@ class Tx_Rbac_Service_ZendAccessControlService implements Tx_Rbac_Interface_Acce
 								}
 								// actions for selected resource Objects
 								$actions = Tx_Extbase_Utility_Arrays::trimExplode(',', $ruleValues['actions'],TRUE);
+
 								if(!isset($ruleValues['allowed']) || $ruleValues['allowed']){
-									$acl->allow(strtolower(trim($roleName)),strtolower(trim($ruleObject)),$actions);
-									//t3lib_div::debug('allow role: '.strtolower(trim($roleName)).' resource: '. strtolower(trim($ruleObject)) .' actions: '.implode(',', $actions)."\n");echo "<br />";
+									if($actions[0] == '*') {
+										$acl->allow(strtolower(trim($roleName)),strtolower(trim($ruleObject)));
+									}else {
+										$acl->allow(strtolower(trim($roleName)),strtolower(trim($ruleObject)),$actions);
+										//t3lib_div::debug('allow role: '.strtolower(trim($roleName)).' resource: '. strtolower(trim($ruleObject)) .' actions: '.implode(',', $actions)."\n");echo "<br />";
+									}
 								} else {
-									$acl->deny(strtolower(trim($roleName)),strtolower(trim($ruleObject)),$actions);
-									//t3lib_div::debug('deny role: '.strtolower(trim($roleName)).' resource: '. strtolower(trim($ruleObject)) .' actions: '.implode(',', $actions)."\n");echo "<br />";
+									if($actions[0] == '*') {
+										$acl->deny(strtolower(trim($roleName)),strtolower(trim($ruleObject)));
+									}else {
+										$acl->deny(strtolower(trim($roleName)),strtolower(trim($ruleObject)),$actions);
+										//t3lib_div::debug('deny role: '.strtolower(trim($roleName)).' resource: '. strtolower(trim($ruleObject)) .' actions: '.implode(',', $actions)."\n");echo "<br />";
+									}
 								}
 							}
 						}
@@ -137,27 +177,57 @@ class Tx_Rbac_Service_ZendAccessControlService implements Tx_Rbac_Interface_Acce
 			}
 
 			$this->userAcl = $acl;
-			return $acl;
-
 		}
 
+		/**
+		 * Return the user ACL to be queried
+		 *
+		 * @return Zend_Acl The user ACL
+		 */
+		protected function getUserAcl() {
+			return $this->userAcl;
+		}
+
+		/**
+		 * Sets the extension name. requered to be able to retrieve the user TS
+		 * without tx_
+		 *
+		 * @param string $extensionName
+		 */
 		public function setExtensionName($extensionName){
 			$this->extensionName = strtolower($extensionName);
 		}
 
+		/**
+		 * Controller plugins settings
+		 * To be able to retrieve page defined roles
+		 *
+		 * @param array $settings
+		 */
 		public function setPluginSettings($settings){
 			$this->pluginSettings = $settings;
 		}
+
+		/**
+		 * Evals one rbac rule
+		 * Each action in a rule is evalueted individually
+		 *
+		 * @param string $rbacRule // @rbacRule ObjectA > new,edit,delete
+		 * @throws Tx_Rbac_Exception_AccessControlServiceException
+		 * @return bool TRUE if the user is allowed for all actions
+		 */
 		protected function evalOneRbacRule($rbacRule){
 			if( !$this->isValidRuleSyntax($rbacRule)) {
 				throw new Tx_Rbac_Exception_AccessControlServiceException('RBAC error: is not valid rule syntax: '. $rbacRule);
 			}
+
 			$rbacRuleObject = $this->getRbacObjectFromRule($rbacRule);
 			$rbacRuleActions = $this->getRbacActionsFromRule($rbacRule);
-			$appliedRole = array_pop($this->getAppliedRoles()); // Retrieve all applied roles but only use the first one
+			$appliedRole = $this->getAppliedRole();
 
-			$isAllowed = FALSE;
-			while (($action=array_pop($rbacRuleActions)) && ($isAllowed == FALSE)) {
+			// If one action in the rule is evaluated false, then return false
+			$isAllowed = TRUE;
+			while (($action=array_pop($rbacRuleActions)) && ($isAllowed == TRUE)) {
 				try {
 					$isAllowed = $this->userAcl->isAllowed($appliedRole,$rbacRuleObject,$action );
 				} catch(Zend_Acl_Role_Registry_Exception $exception) {
@@ -168,6 +238,12 @@ class Tx_Rbac_Service_ZendAccessControlService implements Tx_Rbac_Interface_Acce
 			return $isAllowed;
 		}
 
+		/**
+		 * Evals an array of Rbac rules
+		 *
+		 * @param array $rbacRules
+		 * @return bool TRUE if the user is allowed for all rules
+		 */
 		protected function evalAllRbacRules($rbacRules){
 			$isAllowed = TRUE;
 			while (($rbacRule=array_pop($rbacRules)) && $isAllowed == TRUE) {
@@ -176,6 +252,12 @@ class Tx_Rbac_Service_ZendAccessControlService implements Tx_Rbac_Interface_Acce
 			return $isAllowed;
 		}
 
+		/**
+		 * Sets the fe user to ask for access
+		 *
+		 * @param tslib_feUserAuth $feUser
+		 * @throws Tx_Rbac_Exception_AccessControlServiceException
+		 */
 		protected function setFeUser($feUser) {
 			if(!(get_class($feUser) == 'tslib_feUserAuth') || !($feUser->user['uid'] > 0)){
 				throw new Tx_Rbac_Exception_AccessControlServiceException('error: is not valid FE user of type tslib_feUserAuth');
@@ -183,6 +265,13 @@ class Tx_Rbac_Service_ZendAccessControlService implements Tx_Rbac_Interface_Acce
 			$this->feUser = $feUser;
 		}
 
+		/**
+		 * Tests the RbacRule Syntax
+		 * rbacRule: ObjectA > new,edit,delete
+		 * rbacRule: ObjectA > *
+		 * @param string $rule
+		 * @return bool TRUE if the syntax is OK
+		 */
 		protected function isValidRuleSyntax($rule) {
 			return preg_match('/[[:alnum:]][\t\s]*>([\t\s]*[[:alnum:]*]+[\t\s]*[,]*)+$/', $rule);
 		}
